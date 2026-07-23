bf4428ed122b3879d995e5cdc3043d6b4cf98337
Meet Bhungaliya <meetbhungaliya1900@gmail.com>
2026-07-23 11:55:10 +0530

    build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.29.55"],
   serverExternalPackages: ["@electric-sql/pglite"],
+  output: "standalone",
 };
 
 export default nextConfig;
